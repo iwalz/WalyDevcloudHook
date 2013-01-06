e34443b622b6fa3c4665fd4bf6c29efa7c7dcd08
ingo <ingo.walz@googlemail.com>
2013-01-06 05:28:17 +0100

    First deployment integration

--- library/WalyDevcloudHook/Controller/HookController.php
+++ library/WalyDevcloudHook/Controller/HookController.php
@@ -10,6 +10,7 @@ class HookController extends AbstractActionController
     public function indexAction()
     {
         $request = $this->getRequest();
+
         if ($request->isPost()) {
             $json = $request->getPost('payload');
             $payload = new PayloadAdapter($json);
@@ -26,7 +27,16 @@ class HookController extends AbstractActionController
             unset($xml->parameters);
             file_put_contents('/tmp/test/deployment.xml', (string)$xml->asXML());
             $zdpack->copyFolder($github->getProjectDirectory(), '/tmp/test/data');
-            $zdpack->pack('/tmp/test', '/tmp');
+            $file = $zdpack->pack('/tmp/test', '/tmp');
+
+            $zdpack->deleteFolder('/tmp/test');
+            $zdpack->deleteFolder($github->getProjectDirectory());
+
+            $deployment = new \ZendService\ZendServerAPI\Deployment();
+            $config = $deployment->getPluginManager()->get('config');
+            $deployment->applicationDeploy($file, "http://" . $config->getHost() . '/' . $payload->getRepository()->getName());
+
+            unlink($file);
         }
 
         return false;
