d15cc07b9b69c76ab3edcbc8cf729882161bfd4e
ingo <ingo.walz@googlemail.com>
2013-03-03 00:29:50 +0100

    fixed directories

--- library/WalyDevcloudHook/Factories/LoggerServiceFactory.php
+++ library/WalyDevcloudHook/Factories/LoggerServiceFactory.php
@@ -15,6 +15,7 @@ class LoggerServiceFactory implements FactoryInterface
         $loggerConfig = $config['devcloud_hook']['logger'];
         $logger = new Logger();
 
+        var_dump($loggerConfig['logdir']);
         chmod($loggerConfig['logdir'], 0755);
         $stream = new Stream($loggerConfig['logdir'].'/trigger-'.date('Y-m-d').'.log');
         $filter = new Priority($loggerConfig['loglevel']);
